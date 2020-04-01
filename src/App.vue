f8450fce854fecdbcff2953906ad5c97f5022bff
Neptuniam <ljones1997@hotmail.com>
2020-04-01 17:40:30 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <h1 class="nospacing"> <em>News that matters to you</em> </h1>
+    <h4 class="nospacing">Search for a topic that interests you</h4>
+
+    <div class="searchRow" uk-sticky>
+        <input type="text" v-model="searchTerm" class="uk-input termInput">
+        <select class="uk-select" v-model="sortBy">
+            <option value="publishedAt">Publish Date</option>
+            <option value="popularity">Popular</option>
+        </select>
+        <button type="button" class="uk-button uk-button-primary" @click="requestNews(searchTerm, sortBy)">Search</button>
+
+        <!-- <button v-if="newsArticles" type="button" class="uk-button uk-button-secondary">Save this Term</button> -->
+    </div>
+
+
+    <div v-if="newsArticles">
+        <h3>Headlines:</h3>
+
+        <div v-if="newsArticles.totalResults == 0 || !(newsArticles.articles && newsArticles.articles.length)">
+            No Articles Found
+        </div>
+        <div v-else>
+            <div v-for="(article, index) in newsArticles.articles" :key="index" class="ArticleContainer">
+                <Article :article="article" />
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Article from './components/Article.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Article
+    },
+
+    data() {
+        return {
+            searchTerm: "Technology",
+            sortBy: "publishedAt",
+            newsArticles: null
+        }
+    },
+
+    methods: {
+        requestNews(searchTerm, sortBy) {
+            // Api Can't handle search terms with whitespace
+            searchTerm = searchTerm.trim().replace(/ /g, '')
+
+            let query = `https://newsapi.org/v2/everything?q=${searchTerm}&sortBy=${sortBy}&apiKey=2b056b1596eb4356a56510c4e19da2b7`
+            this.axios.get(query).then(response => {
+                console.log(response);
+
+                if (response.status == 200)
+                    this.newsArticles = response.data
+            })
+        }
+    },
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* text-align: center; */
+    color: #2c3e50;
+    /* margin-top: 60px; */
+    padding: 100px 200px;
 }
+
+    h1, em {
+        color: black !important;
+    }
+    h4 {
+        color: grey;
+    }
+
+    .searchRow {
+        margin-top: 20px;
+    }
+    .searchRow input {
+        width: 300px;
+    }
+    .searchRow select {
+        width: 150px;
+    }
+
+    .ArticleContainer {
+        margin: 100px 0;
+    }
+
+
 </style>
---
+++ src/components/Article.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="">
+    <!-- Articles use special entities in their content and using v-html allows us to properly display those -->
+    <div class="headlineTitle" v-html="title" />
+
+    <hr>
+
+    <p class="headlineContent" v-html="content" />
+
+    <p class="headlineSrc">
+        <b>Article By</b>: {{article.source.name}}
+        <br>
+        <a :href="article.url" target="_blank">Read full article.</a>
+        -
+        <a @click="saveArticle">Save article.</a>
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['article',],
+
+    computed: {
+        title() {
+            if (this.article && this.article.title)
+                return this.article.title.split(' -')[0]
+            return null
+        },
+        content() {
+            if (this.article && this.article.content)
+                return this.article.content.split('[+')[0]
+            return null
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .headlineTitle {
+        font-weight: 800px;
+        font-size: 28px;
+    }
+
+    .headlineContent {
+        font-weight: 400px;
+        font-size: 18px;
+    }
+
+    .headlineSrc {
+        font-weight: 300px;
+        font-size: 14px;
+    }
+
+    .headlineSrc a {
+        color: blue;
+    }
+</style>
